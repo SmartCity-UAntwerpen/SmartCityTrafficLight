73ce161f91d04fd60143c537f2e3a287b524521c
wartek69 <alextcher@hotmail.com>
2018-11-26 17:01:54 +0100

    Deleted .d and .o files
    Cleaning up the directory

--- SmartTrafficLight/serversocket.d
+++
@@ -1,4 +0,0 @@
-serversocket.o: serversocket.c serversocket.h \
- /home/robotdev/minimod/Libs/lin-delay.h \
- /home/robotdev/minimod/Libs/datatypes.h project.h config.h \
- eventpublisher.h msgqueue.h
