df1821bc90a447f055eba2fa2f1ac146ffb49253
wartek69 <alextcher@hotmail.com>
2018-11-26 16:54:41 +0100

    Deleted useless files

--- SmartTrafficLight/project.h
+++ SmartTrafficLight/project.h
@@ -1,21 +1,6 @@
 #ifndef PROJECT_H
 #define PROJECT_H
 
-#define PI
-//#define IGEP
-
-#ifdef IGEP
-#define SER_PORT "/dev/ttyO1"
-#endif
-
-#ifdef PI
-#define SER_PORT "/dev/ttyAMA0"
-#endif
-
-#define RS485_DATABUFFER_SIZE 256
-#define RS485_TXRXBUFFER_SIZE RS485_DATABUFFER_SIZE+64
-#define RS485CLIENT_MAX_BLOCK_SIZE 64
-
-#define APP_VERSION "0.0.3"
+#define APP_VERSION "0.0.4"
 
 #endif
